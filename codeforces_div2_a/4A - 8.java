66924ca4aee2a7f372569ca7398f819476ac7779
karishma-khan <68844209+karishma-khan@users.noreply.github.com>
2021-07-25 20:32:23 +0530

    A. Watermelon

---
+++ codeforces_div2_a/4A - 8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      int w=sc.nextInt();
+      if(w%2!=0||w==2)
+      {
+        System.out.println("NO");   
+      }
+        else
+      System.out.println("YES");
+    }
+}
